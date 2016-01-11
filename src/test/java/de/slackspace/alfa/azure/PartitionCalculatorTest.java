3ca9fd010df18f7b365ec3ebcfeebf28f980e8d5
cternes <github@slackspace.de>
2016-01-11 18:20:17 +0100

    Fixed test by making it timezone independent

--- src/test/java/de/slackspace/alfa/azure/PartitionCalculatorTest.java
+++ src/test/java/de/slackspace/alfa/azure/PartitionCalculatorTest.java
@@ -3,6 +3,7 @@ package de.slackspace.alfa.azure;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import org.junit.Test;
 
@@ -16,11 +17,11 @@ public class PartitionCalculatorTest {
 	
 	@Test
 	public void testPartitionKeyGeneration() {
-		Calendar cal = Calendar.getInstance();
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 		cal.set(2013, 10, 11, 0, 0, 0);
 		cal.set(Calendar.MILLISECOND, 0);
 		
 		String key = PartitionCalculator.calculatePartitionKeyFor(cal);
-		assertEquals("0635197212000000000", key);
+		assertEquals("0635197248000000000", key);
 	}
 }
